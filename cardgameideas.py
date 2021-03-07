f846762c5cd7fb4191ebccfc7ab6ae1255f63fae
Peekoe <peekoecode@gmail.com>
2021-03-06 22:54:22 -0800

    Create cardgameideas.py

---
+++ cardgameideas.py
@@ -0,0 +1,50 @@
+'''
+Trading card game thing
+'''
+
+
+class Player:
+    pass
+
+class Card:
+    pass
+
+'''
+card types:
+monster/creature
+spells
+mana(?) or maybe do per turn hearthstone mana
+'''
+
+'''
+magic:
+creatures
+ -attributes like: lifesteal, rush or whatever it's called
+spells/instants/enchantments
+land
+planeswalkers
+weapons
+
+pokemon:
+pokemon
+items?
+energy
+
+yugioh:
+monsters
+spell cards
+trap cards
+'''
+
+'''
+unos rules:
+match color or face value
+skip 1 person for skip card
+switch changes turn order
++2 and skip turn/+4 and change color
+any color
+double stack if same card (optional)
+0 and 7's rules (optional)
+draw until you can play (optional)
+'''
+
