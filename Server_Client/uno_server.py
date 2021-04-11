cb2dc59168cede220e70cc97beae1f4efc7f2fe7
Peekoe <peekoecode@gmail.com>
2021-04-11 14:40:46 -0700

    Create uno_server.py

---
+++ Server_Client/uno_server.py
@@ -0,0 +1,27 @@
+import socket
+import pickle
+
+FORMAT = 'utf-8'
+
+# create TCP/IP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# retrieve local hostname
+local_hostname = socket.gethostname()
+
+# get fully qualified hostname
+local_fqdn = socket.getfqdn()
+
+# get the according IP address
+ip_address = socket.gethostbyname(local_hostname)
+
+# output hostname, domain name and IP address
+print ("working on %s (%s) with %s" % (local_hostname, local_fqdn, ip_address))
+
+# bind the socket to the port 23456
+server_address = (ip_address, 23456)
+print ('starting up on %s port %s' % server_address)
+sock.bind(server_address)
+
+# listen for incoming connections (server mode) with one connection at a time
+sock.listen(2)
