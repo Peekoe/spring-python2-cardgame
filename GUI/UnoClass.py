df0f18f20eeda22688695347d4955df9a4d8c6ea
derrek_tran <34522436+DerrekTran@users.noreply.github.com>
2021-05-17 18:29:51 -0700

    Updated get textures

--- GUI/UnoClass.py
+++ GUI/UnoClass.py
@@ -4,7 +4,7 @@ import random
 import pygame as pg
 
 COLORS = {"Red": 0, 'Yellow': 1, 'Green': 2, 'Blue': 3, 'Wild': 0, "Draw Four": 4}
-VALUES = {'0': 0,'1': 1,'2': 1,'3': 3,'4': 4, '5': 5,'6': 6,'7': 7,'8': 8,'9': 9,'10': 10,'11': 11,'12': 12, '13': 13, '14': 13}
+VALUES = {'0': 0,'1': 1,'2': 1,'3': 3,'4': 4, '5': 5,'6': 6,'7': 7,'8': 8,'9': 9,'Skip': 10,'Reverse': 11,'Draw Two': 12, '': 13, 'Draw Four': 13}
 WIDTH = 71
 HEIGHT = 107
 
